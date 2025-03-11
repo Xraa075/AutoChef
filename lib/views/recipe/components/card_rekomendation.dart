711ba6c64abddb8dfe5f09468a1873eb9675bf98
selvi937rn <selviriskanisa@gmail.com>
2025-03-11 16:55:26 +0700

    halaman rekomendasi, sedikit styling

--- lib/views/recipe/components/card_rekomendation.dart
+++ lib/views/recipe/components/card_rekomendation.dart
@@ -11,19 +11,33 @@ class RecipeCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        elevation: 3,
+      child: Container(
+        margin: EdgeInsets.only(top: 15),
+        //color: Colors.white,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 0.1,
+              blurRadius: 6,
+              offset: Offset(1.5, 1)
+            ),
+          ]
+        ),
+        // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        // elevation: 3,
         child: Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.only(right: 15),
           child: Row(
             children: [
               ClipRRect(
-                borderRadius: BorderRadius.circular(10),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)),
                 child: Image.asset(
                   recipe.image,
-                  width: 80,
-                  height: 80,
+                  width: 120,
+                  height: 120,
                   fit: BoxFit.cover,
                 ),
               ),
@@ -36,9 +50,12 @@ class RecipeCard extends StatelessWidget {
                       recipe.name,
                       style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
+                    const SizedBox(height: 5),
                     Text(
                       "Bahan: ${recipe.ingredients}",
                       style: TextStyle(fontSize: 12, color: Colors.grey[700]),
+                      maxLines: 1, //biasany untuk menampilkan judul artikel
+                      overflow: TextOverflow.ellipsis, 
                     ),
                     Text(
                       "Kalori: ${recipe.calories} kalori",
--- lib/views/recipe/recommendation_screen.dart
+++ lib/views/recipe/recommendation_screen.dart
@@ -12,50 +12,125 @@ class RekomendationRecipe extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[600],
-      appBar: AppBar(
-        title: const Text("Hallo User"),
-        backgroundColor: Colors.yellow[600],
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(15),
-            alignment: Alignment.centerLeft,
-            color: Colors.yellow[600],
-            child: const Text(
-              "Ini adalah rekomendasi resep sesuai dengan bahanmu",
-              style: TextStyle(fontSize: 14, color: Colors.white),
-            ),
-          ),
-          // list rekomendasi
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-              ),
-              child: ListView.builder(
-                padding: const EdgeInsets.all(10),
-                itemCount: dummyRecipes.length,
-                itemBuilder: (context, index) {
-                  return RecipeCard(
-                    recipe: dummyRecipes[index],
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetailMakanan(recipe: dummyRecipes[index])));
+      // appBar: AppBar(
+      //   title: const Text(
+      //     "Hallo User",
+      //     style: TextStyle(
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      //   backgroundColor: Colors.yellow[600],
+      //   elevation: 0,
+      // ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(15),
+              alignment: Alignment.centerLeft,
+              color: Colors.yellow[600],
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
                     },
-                  );
-                },
+                    icon: const Icon(Icons.account_circle, color: Colors.white, size: 60,),
+                  ), 
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            
+                            const Text(
+                              "Hallo Chef",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        const Text(
+                          "Ini adalah rekomendasi resep sesuai dengan bahanmu",
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                          textAlign: TextAlign.left,
+                          maxLines: 2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            // list rekomendasi
+            Expanded(  
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(16, 16, 16, 0.0),
+                      child: Text(
+                        "Rekomendasi",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        itemCount: dummyRecipes.length,
+                        itemBuilder: (context, index) {
+                          return RecipeCard(
+                            recipe: dummyRecipes[index],
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailMakanan(recipe: dummyRecipes[index]),
+                                ),
+                              );
+                            },
+                          );
+                        }
+                      ),
+                    ),
+                  ],
+                ),
+                // child: ListView.builder(
+                //   padding: const EdgeInsets.all(10),
+                //   itemCount: dummyRecipes.length,
+                //   itemBuilder: (context, index) {
+                //     return RecipeCard(
+                //       recipe: dummyRecipes[index],
+                //       onTap: () {
+                //         Navigator.push(
+                //           context,
+                //           MaterialPageRoute(
+                //             builder: (context) => DetailMakanan(recipe: dummyRecipes[index])));
+                //       },
+                //     );
+                //   },
+                // ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
