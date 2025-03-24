874c02ad0411de2a3ce57340005539f0dd34f9e3
selvi937rn <selviriskanisa@gmail.com>
2025-03-24 12:57:10 +0700

    laucher-persiapan rilis

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.autochef"
+        applicationId = "com.autochef.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
