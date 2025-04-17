117a6ae0ec009419fe07b9402b80f53ce8d44144
rnnfl <renno.hangara@gmail.com>
2025-04-17 21:06:10 +0700

    u numbering

--- lib/views/recipe/components/steps.dart
+++ lib/views/recipe/components/steps.dart
@@ -3,25 +3,30 @@ import 'package:flutter/material.dart';
 class Steps extends StatelessWidget {
   final List<String> steps;
 
-  Steps({required this.steps});
+  const Steps({super.key, required this.steps});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Langkah-langkah", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        ...steps.map((step) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 5),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("✓ ", style: TextStyle(fontSize: 16,color: const Color.fromARGB(255, 255, 187, 0), fontWeight: FontWeight.bold)),
-                  Expanded(child: Text(step, style: TextStyle(fontSize: 16))),
-                ],
-              ),
-            )),
+        Text(
+          'Langkah-langkah',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        ...List.generate(
+          steps.length,
+          (index) {
+            final step = steps[index].trim();
+            if (step.isEmpty) return SizedBox(); // skip langkah kosong
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6.0),
+              child: Text('${index + 1}. $step'),
+            );
+          },
+        ),
       ],
     );
-  } 
+  }
 }
